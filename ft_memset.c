767d3eb38d0b59bd50a9d00ff3b98685ab4ef45c
Thomas Millot <thomas-millot@hotmail.com>
2024-10-15 16:32:32 +0200

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -1,19 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmillot <tmillot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/15 13:51:13 by tmillot           #+#    #+#             */
-/*   Updated: 2024/10/15 16:29:57 by tmillot          ###   ########.fr       */
+/*   Created: 2024/10/15 15:17:43 by tmillot           #+#    #+#             */
+/*   Updated: 2024/10/15 16:29:41 by tmillot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stddef.h>
-# include <stdio.h>
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*dest;
 
-#endif
+	dest = (unsigned char *)s;
+	while (n > 0)
+	{
+		*dest++ = (unsigned char)c;
+		n--;
+	}
+	return (s);
+}
+
+/*int main(int argc, char **argv)
+{
+	(void)argc;
+	ft_memset(argv[1], argv[2][0], 5);
+	printf("%s", argv[1]);
+}*/
