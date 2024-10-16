b9dabfb4c9b67de8648bfc774623dd88fdd9b0f7
Thomas Millot <thomas-millot@hotmail.com>
2024-10-16 18:16:01 +0200

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,33 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmillot <tmillot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/15 15:17:43 by tmillot           #+#    #+#             */
-/*   Updated: 2024/10/16 17:30:03 by tmillot          ###   ########.fr       */
+/*   Created: 2024/10/16 16:40:40 by tmillot           #+#    #+#             */
+/*   Updated: 2024/10/16 18:15:29 by tmillot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *s, int c, size_t n)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	unsigned char	*dest;
+	size_t	i;
+	size_t	len;
 
-	dest = (unsigned char *)s;
-	while (n > 0)
+	len = 0;
+	while (src[len] != '\0')
+		len++;
+	i = 0;
+	if (size > 0)
 	{
-		*dest++ = (unsigned char)c;
-		n--;
+		while (i < size - 1 && src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	return (s);
+	return (len);
 }
-
-/*int main(int argc, char **argv)
-{
-	(void)argc;
-	ft_memset(argv[1], argv[2][0], 5);
-	printf("%s", argv[1]);
-}*/
