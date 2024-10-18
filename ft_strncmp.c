f1a143ff85630e6ec7b4a74e40f743a0225cf37f
Thomas Millot <thomas@MacBook-Pro.local>
2024-10-18 17:53:43 +0200

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -2,9 +2,11 @@
 
 int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    int i;
+    size_t i;
 
     i = 0;
+    if (n == 0)
+        return (0);
     while (s1[i] == s2[i] && (i < n - 1) && (s1[i] || s2[i]))
         i++;
     return (s1[i] - s2[i]);
@@ -12,6 +14,7 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 
 /*int main(int argc, char **argv)
 {
+    (void)argc;
     printf("%d\n", strncmp(argv[1], argv[2], atoi(argv[3])));
     printf("%d", ft_strncmp(argv[1], argv[2], atoi(argv[3])));
 }*/
