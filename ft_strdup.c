cd66b54f6ec5a1663b3ac646bc5fb91d69597810
Thomas Millot <thomas-millot@hotmail.com>
2024-10-21 15:49:12 +0200

    strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,26 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmillot <tmillot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/15 13:51:13 by tmillot           #+#    #+#             */
-/*   Updated: 2024/10/21 15:37:22 by tmillot          ###   ########.fr       */
+/*   Created: 2024/10/21 15:21:15 by tmillot           #+#    #+#             */
+/*   Updated: 2024/10/21 15:48:56 by tmillot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stddef.h>
-# include <stdio.h>
-# include <string.h>
-# include <stdlib.h>
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	int		i;
 
-size_t	ft_strlen(const char *s);
-void	ft_bzero(void *s, size_t n);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+	i = 0;
+	dup = malloc(ft_strlen(s) * sizeof(char) + 1);
+	if (!dup)
+		return (0);
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
 
-#endif
+/*int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s", ft_strdup((const char *)argv[1]));
+}*/
