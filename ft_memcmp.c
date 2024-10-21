ebbe3b576a9a9302cd723801e4ec5e73b6eb606c
Thomas Millot <thomas-millot@hotmail.com>
2024-10-21 11:49:16 +0200

    memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,34 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmillot <tmillot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/21 09:47:10 by tmillot           #+#    #+#             */
-/*   Updated: 2024/10/21 10:48:45 by tmillot          ###   ########.fr       */
+/*   Created: 2024/10/21 11:04:24 by tmillot           #+#    #+#             */
+/*   Updated: 2024/10/21 11:40:31 by tmillot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*src;
-	size_t			i;
-
-	src = (unsigned char *)s;
-	i = 0;
-	while (src[i] != c && i < n)
-		i++;
-	if (src[i] == c)
-		return ((void *)&(src[i]));
-	return (NULL);
+	if (!n)
+		return (0);
+	while (n - 1 > 0 && *(const char *)s1 == *(const char *)s2 && (s1 || s2))
+	{
+		s1++;
+		s2++;
+		n--;
+	}
+	return (*(const char *)s1 - *(const char *)s2);
 }
 
 /*int main(int argc, char **argv)
 {
-	(void)argc;
-    printf("%s\n", (char *)memchr(argv[1], argv[2][0], atoi(argv[3])));
-    printf("%s\n", (char *)ft_memchr(argv[1], argv[2][0], atoi(argv[3])));
+    (void)argc;
+    printf("%d\n", memcmp(argv[1], argv[2], atoi(argv[3])));
+    printf("%d", ft_memcmp(argv[1], argv[2], atoi(argv[3])));
 }*/
