9edabd7bef519ebac2915355fb874bb5ffb54e94
Thomas Millot <thomas-millot@hotmail.com>
2024-10-22 13:27:39 +0200

    substr

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tmillot <tmillot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 14:51:35 by tmillot           #+#    #+#             */
-/*   Updated: 2024/10/21 15:20:14 by tmillot          ###   ########.fr       */
+/*   Updated: 2024/10/22 13:19:40 by tmillot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,8 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	void	*ptr;
+	char	*ptr;
 
-	if (nmemb && size)
-		return (0);
 	ptr = malloc(nmemb * size);
 	if (ptr)
 		ft_bzero(ptr, (nmemb * size));
