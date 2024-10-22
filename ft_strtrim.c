2b50f68e9370c33196c045ebe7e925389eac3bee
Thomas Millot <thomas-millot@hotmail.com>
2024-10-22 16:55:18 +0200

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,32 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmillot <tmillot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/18 10:52:36 by tmillot           #+#    #+#             */
-/*   Updated: 2024/10/22 15:15:05 by tmillot          ###   ########.fr       */
+/*   Created: 2024/10/22 14:57:39 by tmillot           #+#    #+#             */
+/*   Updated: 2024/10/22 16:55:03 by tmillot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	int	i;
+	int		start;
+	int		end;
 
-	i = 0;
-	while (s[i] != '\0' && s[i] != (char)c)
-		i++;
-	if (s[i] == (char)c)
-		return ((char *)&s[i]);
-	return (NULL);
+	start = 0;
+	end = ft_strlen(s1);
+	while (ft_strchr(set, s1[start]))
+		start++;
+	while (ft_strchr(set, s1[end]))
+		end--;
+	return (ft_substr(s1, start, (end - start + 1)));
 }
 
 /*int main(int argc, char **argv)
 {
 	(void)argc;
-	printf("%s\n", strchr(argv[1], argv[2][0]));
-	printf("%s", ft_strchr(argv[1], argv[2][0]));
+	char *str;
+	str = ft_strtrim(argv[1], argv[2]);
+	printf("%s", str);
 }*/
